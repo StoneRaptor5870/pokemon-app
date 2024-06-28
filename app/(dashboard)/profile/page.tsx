c4b46240ff7661915329d848fb0420ac334682b8
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-06-28 16:14:22 +0530

    pokemon card

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../../lib/auth";
 import { redirect } from "next/navigation";
 import prisma from "@/prisma/db";
+import { Card } from "@/components/ui/card";
 
 const userData = async () => {
   const session = await getServerSession(authOptions);
@@ -28,34 +29,58 @@ export default async function Profile() {
   const { name, email, firstPokemon } = userDataAndPokemon;
 
   return (
-    <div className="p-4 bg-white">
-      <h1 className="text-2xl font-bold">User Profile</h1>
-      <div className="mt-4">
-        <p>
-          <strong>Name:</strong> {name}
-        </p>
-        <p>
-          <strong>Email:</strong> {email}
-        </p>
-        <h2 className="mt-4 text-xl font-semibold">First Pokémon</h2>
-        {firstPokemon ? (
-          <div className="mt-2">
-            <p>
-              <strong>Name:</strong> {firstPokemon.pokemonName}
-            </p>
-            <p>
-              <strong>Id:</strong> {firstPokemon.pokemonId}
-            </p>
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <Card title="First Pokémon">
+        <div className="flex flex-col justify-center items-center shadow-md p-4">
+          <p>
+            Name: {firstPokemon?.pokemonName}
+          </p>
+          <p>
+            Id: {firstPokemon?.pokemonId}
+          </p>
+          {firstPokemon?.pokemonImage ? (
             <img
-              src={firstPokemon.pokemonImage ?? ''}
-              alt={firstPokemon.pokemonName}
+              src={firstPokemon?.pokemonImage}
+              alt={firstPokemon?.pokemonName}
               className="w-32 h-32"
             />
-          </div>
-        ) : (
-          <p>No Pokémon found</p>
-        )}
-      </div>
+          ) : (
+            <p>No image available</p>
+          )}
+        </div>
+      </Card>
     </div>
-  );
+  )
+
+  // return (
+  //   <div className="p-4 bg-white">
+  //     <h1 className="text-2xl font-bold">User Profile</h1>
+  //     <div className="mt-4">
+  //       <p>
+  //         <strong>Name:</strong> {name}
+  //       </p>
+  //       <p>
+  //         <strong>Email:</strong> {email}
+  //       </p>
+  //       <h2 className="mt-4 text-xl font-semibold">First Pokémon</h2>
+  //       {firstPokemon ? (
+  //         <div className="mt-2">
+  //           <p>
+  //             <strong>Name:</strong> {firstPokemon.pokemonName}
+  //           </p>
+  //           <p>
+  //             <strong>Id:</strong> {firstPokemon.pokemonId}
+  //           </p>
+  //           <img
+  //             src={firstPokemon.pokemonImage ?? ''}
+  //             alt={firstPokemon.pokemonName}
+  //             className="w-32 h-32"
+  //           />
+  //         </div>
+  //       ) : (
+  //         <p>No Pokémon found</p>
+  //       )}
+  //     </div>
+  //   </div>
+  // );
 }
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,27 +2,63 @@
 
 import { Card } from "./ui/card";
 
-const Dashboard: React.FC = () => {
+interface PokemonData {
+  id: string;
+  pokemonId: number;
+  pokemonName: string;
+  pokemonImage: string | null;
+  userId: string;
+}
+
+const Dashboard: React.FC<{ pokemonData: PokemonData }> = ({ pokemonData }) => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      {/* <Card title="Home">
-        <div className="flex flex-col justify-center items-center">
-          <div className="w-full p-4">
-            <p className="text-xl">User Profile</p>
-            <div>
-              This feature lets you see your favoutrite pokemons that the user has saved for them.
-            </div>
-          </div>
-          <div className="w-full p-4">
-            <p className="text-xl">Search Pokemons</p>
-            <div>
-              This feature lets search pokemons on the basis of name, region, types etc.
-            </div>
-          </div>
-        </div>
-      </Card> */}
+      <div className="flex flex-col justify-center items-center m-auto mt-64">
+        {pokemonData.pokemonImage ? (
+          <img
+            src={pokemonData.pokemonImage}
+            alt={pokemonData.pokemonName}
+            className="w-64 h-64 m-auto"
+          />
+        ) : (
+          <p>No image available</p>
+        )}
+      </div>
     </div>
-  )
+  );
 };
+
+// const Dashboard = async ({
+//   pokemonData,
+// }: {
+//   pokemonData: {
+//     id: string;
+//     pokemonId: number;
+//     pokemonName: string;
+//     pokemonImage: string | null;
+//     userId: string;
+//   }
+// }) => {
+//   return (
+//     <div className="flex flex-col justify-center items-center w-full h-full">
+//       {/* <Card title="Home">
+//         <div className="flex flex-col justify-center items-center">
+//           <div className="w-full p-4">
+//             <p className="text-xl">User Profile</p>
+//             <div>
+//               This feature lets you see your favoutrite pokemons that the user has saved for them.
+//             </div>
+//           </div>
+//           <div className="w-full p-4">
+//             <p className="text-xl">Search Pokemons</p>
+//             <div>
+//               This feature lets search pokemons on the basis of name, region, types etc.
+//             </div>
+//           </div>
+//         </div>
+//       </Card> */}
+//     </div>
+//   );
+// };
 
 export default Dashboard;

b9925f9dc8ba2b4fce96236a8ecd8d4297d171e8
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-07-02 15:15:35 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
   const session = await getServerSession(authOptions);
 
   if (!session?.user) {
-    redirect("/signin");
+    redirect("/signup");
   } else {
     redirect("/landing")
   }
